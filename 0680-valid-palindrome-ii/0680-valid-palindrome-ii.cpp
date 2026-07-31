9abef124258075f2705b4adb0d972b1afe7ddd43
Piyush Kumar Rai <69131562+PiyushRai98@users.noreply.github.com>
2026-07-31 08:10:42 +0530

    Time: 7 ms (28.9%), Space: 22.1 MB (77.78%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool palindromeHelper(int i, int j, string &s){
+        while (i<j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+       return true; 
+    }
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            char left = s[i];
+            char right = s[j];
+
+            if(left != right){
+                return palindromeHelper(i+1, j, s)||palindromeHelper(i, j-1, s);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
